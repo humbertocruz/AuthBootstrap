68d4a94c242936a64826cea0f0cf8a6919c9654f
Humberto Cruz <humberto.cruz@gmail.com>
2014-01-28 14:45:17 -0200

    login

--- Ab/Controller/AbLoginsController.php
+++ Ab/Controller/AbLoginsController.php
@@ -41,13 +41,12 @@ class AbLoginsController extends AbAppController {
 
 		$isLogged = $this->Auth->loggedIn();
 		$sistema_id = $this->request->query['sistema_id'];
-		
 		if ( !$isLogged ) {
 			//$this->redirect('/login?sistema_id='.$sistema_id);
 		} else {
 			$usuario = $this->Auth->user();
 			$sistema_id = $this->request->query['sistema_id'];
-
+			echo 'ok';
 			$AbSistema = $this->AbSistema->read(null, $sistema_id);
 			$this->set('sistema_url', $AbSistema['AbSistema']['url'].'/authenticate');
 			
